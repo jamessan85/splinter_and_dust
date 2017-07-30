4b25c72ef4fef859eca19b060552446422426d60
James Sanderson <jamessandersoon@gmail.com>
2017-07-30 22:36:46 +0100

    Chnage to DB URL

--- settings/staging.py
+++ settings/staging.py
@@ -4,7 +4,7 @@ import dj_database_url
 DEBUG = False
 
 DATABASES = {
-    'default': dj_database_url.config('mysql://b91e600fa9d9e1:b19256f2@eu-cdbr-west-01.cleardb.com/heroku_311bf5ff6bb89d3?')
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 # Stripe environment variables
