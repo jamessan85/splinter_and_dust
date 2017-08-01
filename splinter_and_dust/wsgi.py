e4e6d9a1481ba6ca11e50163ccb9a134745f5caa
James Sanderson <jamessandersoon@gmail.com>
2017-08-01 21:12:22 +0100

    add whitenoise

--- splinter_and_dust/wsgi.py
+++ splinter_and_dust/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "splinter_and_dust.settings")
 
 application = get_wsgi_application()
-
+application = DjangoWhiteNoise(application)
