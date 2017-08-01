61f902ede7dac77d5ff8b022d26b291babc9fc4d
James Sanderson <jamessandersoon@gmail.com>
2017-08-01 12:52:41 +0100

    created staticfiles

---
+++ staticfiles/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 27/07/2017.
+ */
+
+$(document).ready(function () {
+     $(".hamburger").click(function () {
+        $(".navibar").slideToggle().css('display','flex');
+    });
+
+});
