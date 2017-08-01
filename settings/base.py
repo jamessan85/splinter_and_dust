927c64c03ba2ba65db79b4351793d998485d9f39
James Sanderson <jamessandersoon@gmail.com>
2017-08-01 09:47:57 +0100

    Chnaged static root

--- settings/base.py
+++ settings/base.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = ''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -141,7 +141,5 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'tradelogins.backends.EmailAuth',
 )
-
-
 
 TINYMCE_JS_ROOT = os.path.join(BASE_DIR, "static", 'js', 'tinymce', 'tinymce.min.js')
