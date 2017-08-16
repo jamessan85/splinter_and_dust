7a60dc4162397d379d83120126e3c3b0d8e619b6
James Sanderson <jamessandersoon@gmail.com>
2017-08-16 20:07:34 +0100

    Chnages made to responsive site

--- static/js/main.js
+++ static/js/main.js
@@ -7,4 +7,22 @@ $(document).ready(function () {
         $(".navibar").slideToggle().css('display','flex');
     });
 
+    $("#button1").click(function () {
+        $("#dropdown-content1").slideToggle().css('display','block');
+         $("#dropdown-content2").slideToggle().css('display','none');
+         $("#dropdown-content3").slideToggle().css('display','none');
+    });
+
+    $("#button2").click(function () {
+        $("#dropdown-content2").slideToggle().css('display','block');
+        $("#dropdown-content1").slideToggle().css('display','none');
+        $("#dropdown-content3").slideToggle().css('display','none');
+    });
+
+    $("#button3").click(function () {
+        $("#dropdown-content3").slideToggle().css('display','block');
+        $("#dropdown-content1").slideToggle().css('display','none');
+        $("#dropdown-content2").slideToggle().css('display','none');
+    });
+
 });
