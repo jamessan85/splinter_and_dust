2a1fb197c19cab63a98baa8c6581dd90191ef970
James Sanderson <jamessandersoon@gmail.com>
2017-08-25 22:25:24 +0100

    change to URLs

--- splinter_and_dust/urls.py
+++ splinter_and_dust/urls.py
@@ -23,17 +23,23 @@ from django.views.generic import TemplateView
 from settings.base import MEDIA_ROOT
 
 urlpatterns = [
+    #admin site
     url(r'^admin/', admin.site.urls),
+    #home page
     url(r'^$', products_views.product_display, name='home'),
+    #collections
     url(r'^collection/(?P<collection>\w+)/$', products_views.sort_by_collection, name='collection'),
+    #company information
     url(r'^company/$', products_views.show_companies, name='companiesall'),
     url(r'^company/(?P<company>\d+)/$', products_views.sort_by_company, name='companies'),
+    #prdouct informations
     url(r'^products/(?P<id>\d+)/$', products_views.product_detail, name='productbyid'),
     url(r'^products/$', products_views.products_by_user, name='productsbyuser'),
     url(r'^products/purchase/(?P<purchase>\d+)/$', products_views.purchase, name='purchase'),
     url(r'^products/edit/(?P<edit_prod>\d+)/$', products_views.edit_product, name='editproduct'),
     url(r'^register/$', tradelogin_views.register, name='register'),
     url(r'^register/cust/$', tradelogin_views.registercust, name='custregister'),
+    #profile views
     url(r'^profile/$', tradelogin_views.profile, name='profile'),
     url(r'^profile/productsbought/(?P<userid>\d+)/$', products_views.products_bought, name='prodbought'),
     url(r'^product/new/$', products_views.new_product, name='new_product'),
@@ -43,6 +49,7 @@ urlpatterns = [
     url(r'^profile/accountinfo/edit/(?P<account_num>\d+)/$', tradelogin_views.editaccountinfo, name='editaccountinfo'),
     url(r'^cancel_subscription/$', tradelogin_views.cancel_subscription, name='cancel_subscription'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    #media views
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     url(r'^media/banners/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     url(r'^media/logo_square/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
